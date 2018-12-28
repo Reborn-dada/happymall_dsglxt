c3905a19e1d3d55d22600e028376d97bbaceb709
lx <1224662297@qq.com>
2018-12-28 23:10:02 +0800

    通用模块-页头开发

--- src/app.jsx
+++ src/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router,Route,Link,Switch,Redirect} from 'react-router-dom';
 
+import Layout from 'component/layout/index.jsx';
 import Home from 'page/home/index.jsx';
 
 class App extends React.Component{
@@ -9,10 +10,12 @@ class App extends React.Component{
     render(){
         return (
             <Router>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Redirect from="*" to="/"/>
-                </Switch>    
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Redirect from="*" to="/"/>
+                    </Switch>  
+                </Layout>  
             </Router>
         )
     }
