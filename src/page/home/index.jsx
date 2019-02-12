9131c8c03785c4cbf9454e83bd1d7dc8f97b00dd
lx <1224662297@qq.com>
2019-02-12 10:10:31 +0800

    登入功能

--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import './index.css';
-import PageTitle from 'component/page-title/index.jsx'
+import PageTitle from 'component/page-title/index.jsx';
+import {Row,Col,Checkbox} from 'antd';
 
 class Home extends React.Component{
+    onCheckBoxChange(){}
     render(){
+        
         return(
             <div id="page-wrapper">
                  <PageTitle title="首页">
@@ -13,6 +16,7 @@ class Home extends React.Component{
                     <div className="col-md-12">body
                     </div>
                 </div>
+
             </div>
         )
     }
--- src/util/mm.jsx
+++ src/util/mm.jsx
@@ -1,5 +1,37 @@
-class MUtil {
 
+
+class MUtil {
+    request(param){
+        return new Promise((resolve,reject)=>{
+            $.ajax({
+                type    :param.type     || 'get',
+                url     :param.url      || '',
+                dataType:param.dataType || 'json',
+                data    :param.data     || null,
+                //回调函数
+                success :(res)=>{
+                    //请求数据成功
+                    if(0 === res.status){
+                        typeof resolve === 'function' && resolve(res.data,res.msg)
+                    }//请登入
+                    else if (10 === res.status){
+                        this.doLogin()
+                    }
+                    else{
+                        typeof reject === 'function' && reject(res.msg || res.data)
+                    }
+                },
+                error   :(err)=>{
+                    typeof reject === 'function' && reject(err.statusText)
+                }
+            });
+        });
+        
+    }
+
+    doLogin(){
+        window.location.href='/login?redirect='+ encodeURIComponent(window.location.pathname)
+    }
 }
 
 export default MUtil;
