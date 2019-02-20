0ae72ae90b12dee4519841efc95de2f20ee59927
lx <1224662297@qq.com>
2019-02-20 13:49:25 +0800

    登录页面开发

--- src/util/mm.jsx
+++ src/util/mm.jsx
@@ -25,12 +25,55 @@ class MUtil {
                     typeof reject === 'function' && reject(err.statusText)
                 }
             });
-        });
-        
+        }); 
     }
 
     doLogin(){
         window.location.href='/login?redirect='+ encodeURIComponent(window.location.pathname)
+    }
+
+    //获取url参数
+    getUrlParam(name){
+        //?param=123&param1=456
+        let queryString = window.location.search.split('?')[1] || '',
+            reg         = new RegExp("(^|&)" + name + "=([^|&]*)(&|$)"),
+            result      = queryString.match(reg);
+        //result :['param=123','','123','&']
+        return result?decodeURIComponent(result[2]):null;
+    }
+
+    //获取错误提示
+    errorTips(errMsg){
+        alert(errMsg || '好像哪里不对了')
+    }
+    //本地存储
+    setStorage(name,data){
+        let dataType = typeof data;
+        //json对象
+        if(dataType === 'object'){
+            window.localStorage.setItem(name,JSON.stringify(data))
+        }
+        //基本类型
+        else if(['number','string','boolean'].indexOf(dataType) >=0){
+            window.localStorage.setItem(name,data)
+        }
+        //其他不支持类型
+        else{
+            alert('该类型不能用于本地存储')
+        }
+    }
+    //取出存储内容
+    getStorage(name){
+        let data = window.localStorage.getItem(name);
+        if(data){
+            return JSON.parse(data);
+        }else{
+            return '';
+        }
+    }
+    //删除本地村粗
+    removeStorage(name){
+        window.localStorage.removeItem(name);
     }
 }
 
